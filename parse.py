323c35d5e60a20c47e012ca097b106c50229b8d1
Nicolas Kirchhoffer <git@nkirchho.dev>
2022-11-09 23:28:58 +0100

    :sparkles: building dependencies list at the end

--- parse.py
+++ parse.py
@@ -3,6 +3,9 @@ import yaml, json
 yaml_path = "deployment.yaml"
 json_path = "_definitions.json"
 
+dependencies = []
+MAX_DEPTH = 4
+
 def get_definition_properties(uri):
     if uri[0] == '#':
         uri = uri.split('/')[2]
@@ -11,61 +14,54 @@ def get_definition_properties(uri):
 
         try:
             definition = definitions[uri]
-            return (definition["properties"])
+        
+            if 'required' in definition:
+                return (definition['properties'], definition['required'])
+            else:
+                return (definition['properties'], [])
         except:
             print("Definition " + uri + " missing from definitions")
             exit(1)
 
-def dive_manifest(currentNode, currentRef, currentDepth = 0):
-    if type(currentNode) is dict:
-        for key, value in currentNode.items():
-            print("Current ref: " + currentRef)
-            print(' '*currentDepth + key)
-            if type(value) is dict or type(value) is list:
-                definition = get_definition_properties(currentRef)
-                currentNodeProp = definition[key]
 
-                if 'items' in currentNodeProp:
-                  if '$ref' in currentNodeProp["items"]:
-                    dive_manifest(value, currentNodeProp["items"]["$ref"], currentDepth + 1)
-                elif '$ref' in currentNodeProp:
-                  dive_manifest(value, currentNodeProp["$ref"], currentDepth + 1)
-
-
-    elif type(currentNode) is list:
-        for value in currentNode:
-            dive_manifest(value, currentRef, currentDepth + 1)
-
+def dive_manifest(currentNode, currentRef, parentName, currentDepth = 0):
+    if currentDepth <= MAX_DEPTH:
+        if type(currentNode) is dict:
+            (properties, required) = get_definition_properties(currentRef)
+            for key, value in currentNode.items():
+                currentDef = properties[key]
+                if type(value) is str:
+                    if key in required:
+                        dependencies.append({ str(parentName): value, 'depth': currentDepth })
+                    print(dependencies)
+                elif type(value) is dict:
+                    dive_manifest(value, currentDef['$ref'], key, currentDepth + 1)
+                elif type(value) is list:
+                    dive_manifest(value, currentDef['items']['$ref'], key, currentDepth + 1)
+        elif type(currentNode) is list:
+                for value in currentNode:
+                    dive_manifest(value, currentRef, parentName, currentDepth + 1)
 
 def main():
     with open(yaml_path) as file:
         manifest = yaml.safe_load(file)
+
         try:
             kind = manifest["kind"]
             apiVersion = manifest["apiVersion"]
         except:
             print("Kind or apiVersion missing from manifest, quitting")
             exit(1)
-
+        
         if apiVersion == "v1":
-          apiVersion = "core.v1"
+            apiVersion = "core.v1"
         else:
-          apiVersion = apiVersion.replace("/", ".")
+            apiVersion = apiVersion.replace("/", ".")
 
         ref = "io.k8s.api." + apiVersion
-
         uri = ref + "." + kind
 
-        dive_manifest(manifest, uri)
-        
-#        for name, prop in props.items():
-#          if name == "apiVersion" or name == "kind" or name == "metadata":
-#            continue
-#          if name in manifest:
-#            print(prop)
+        dive_manifest(manifest, uri, None)
 
-    
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
